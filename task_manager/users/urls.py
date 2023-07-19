991cf626f9d80acbeebcb74130e3a1fc8ffd0b44
KarinA <abdullaeva.karina.r@gmail.com>
2023-07-19 14:30:51 +0300

    main: Ad CRUD operations for users

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from task_manager.users import views
+from task_manager.users.views import UserListView, \
+                                     UserCreateView, \
+                                     UserUpdateView, \
+                                     UserDeleteView
 
 
 urlpatterns = [
-    path('', views.UserCreateView.as_view(), name='user_create'),
+    path('', UserListView.as_view(), name='user_list'),
+    path('create/', UserCreateView.as_view(), name='user_create'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,8 +1,22 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from .forms import UserForm
 from .models import User
+
+
+class UserListView(ListView):
+    """List of all users."""
+    model = User
+    template_name = 'users.html'
+    context_object_name = 'users'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        users = User.objects.exclude(username='admin')
+        return users
 
 
 class UserCreateView(SuccessMessageMixin, CreateView):
@@ -10,5 +24,31 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     model = User
     form_class = UserForm
     template_name = 'signup.html'
-    success_url = ' '
+    success_url = reverse_lazy('user_list')
     success_message = _('User is successfully registered')
+    extra_context = {
+        'button_text': _('Create'),
+    }
+
+
+class UserUpdateView(SuccessMessageMixin, UpdateView):
+    """Update the user."""
+    model = User
+    form_class = UserForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('user_list')
+    success_message = _('User is successfully updated')
+    extra_context = {
+        'button_text': _('Update'),
+    }
+
+
+class UserDeleteView(SuccessMessageMixin, DeleteView):
+    """Delete the user."""
+    model = User
+    template_name = 'delete.html'
+    success_url = reverse_lazy('user_list')
+    success_message = _('User is successfully deleted')
+    extra_context = {
+        'button_text': _('Delete'),
+    }
