bca1cb0875b8ae5443e7ee2658e136e174027e3e
KarinA <abdullaeva.karina.r@gmail.com>
2023-07-20 18:36:58 +0300

    main: Delete unnecessary code

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser  # , Permission
-# from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import AbstractUser
 
 
 class TimestampedModel(models.Model):
@@ -19,23 +18,3 @@ class User(AbstractUser, TimestampedModel):
 
     def __str__(self) -> str:
         return self.get_full_name()
-
-    # def user_gains_perms(request, user_id):
-    #     user = get_object_or_404(User, pk=user_id)
-    #     # any permission check will cache the current set of permissions
-    #     user.has_perm('users.change_user')
-
-    #     permission = Permission.objects.get(
-    #         codename='change_user',
-    #     )
-    #     user.user_permissions.add(permission)
-
-    #     # Checking the cached permission set
-    #     user.has_perm('users.change_user')  # False
-
-    #     # Request new instance of User
-    #     # Be aware that user.refresh_from_db() won't clear the cache.
-    #     user = get_object_or_404(User, pk=user_id)
-
-    #     # Permission cache is repopulated from the database
-    #     user.has_perm('users.change_user')  # True
