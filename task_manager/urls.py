3295ccd1bea37ce1af70b88267972bb6039e28fa
KarinA <abdullaeva.karina.r@gmail.com>
2023-07-21 22:33:24 +0300

    main: Add urls for tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/', views.UserLogInView.as_view(), name='login'),
     path('logout/', views.UserLogOutView.as_view(), name='logout'),
     path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.statuses.tasks')),
 ]
