71a2b3d52580907656194c554254c5764e57629a
KarinA <abdullaeva.karina.r@gmail.com>
2023-07-31 18:02:10 +0300

    Fix translation

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import CharField
+from django.utils.translation import gettext_lazy as _
 
 from task_manager.users.models import User
 
 
 class UserForm(UserCreationForm):
     """Form for user creation."""
+    first_name = CharField(max_length=150,
+                           required=True,
+                           label=_("First name"))
+    last_name = CharField(max_length=150,
+                          required=True,
+                          label=_("Last name"))
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = (
