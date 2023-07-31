2d24d23dd36aefc2aa113d7899957ea54c2be2c5
KarinA <abdullaeva.karina.r@gmail.com>
2023-07-31 16:42:38 +0300

    main: Favicon

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,7 +20,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -34,7 +33,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 #     'webserver',
 #     '127.0.0.1',
 #     '0.0.0.0',
-#     'task-manager-by-karina.onrender.com/'
+#     'https://task-manager-by-karina.onrender.com/'
 # ]
 
 ALLOWED_HOSTS = ['*']
